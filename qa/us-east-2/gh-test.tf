44b865b80052e4ef806365e2a0a105ab87592634
Yannick <bessongjaimini@gmail.com>
2024-10-30 01:07:00 -0400

    Added AWS creds

--- qa/us-east-2/gh-test.tf
+++ qa/us-east-2/gh-test.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "test-vpc" {
-    cidr_block = "10.11.0.0/16"
-    tags = {
-      Name = "qa vpc"
-      Environment = "qa"
-    }
-  
+  cidr_block = "10.11.0.0/16"
+  tags = {
+    Name        = "qa vpc"
+    Environment = "qa"
+  }
+
 }
